413b3ee8b128ff4d17027f2366a99d95ff77ddc5
wangyunsong <yunsong.wang@xiaoxitech.com>
2025-05-07 10:57:10 +0800

    feat: 添加 tailwindcss 依赖

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="flex flex-row justify-center">
         <a href="https://vite.dev" target="_blank">
           <img src={appLogo} className="logo" alt="witness logo" />
         </a>
