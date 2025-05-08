ebc1f5e1836c17435bce036e7a0d9773969904d3
wangyunsong <yunsong.wang@xiaoxitech.com>
2025-05-08 18:02:36 +0800

    WIP: PWA配置

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), VitePWA({
+    base: '/',
+
     strategies: 'injectManifest',
     srcDir: 'src',
     filename: 'sw.js',
@@ -19,10 +21,28 @@ export default defineConfig({
     },
 
     manifest: {
-      name: 'witness',
-      short_name: 'witness',
+      name: 'Witness',
+      short_name: 'Witness',
+      display: 'standalone',
+      display_override: ["minimal-ui"],
+      id: '/',
+      start_url: '/',
+      // start_url: 'http://10.0.0.55:5173/',
       description: '\u0016\u0016Habit-forming apps',
       theme_color: '#ffffff',
+      background_color: '#000000',
+      icons: [
+        {
+          src: './public/icon-512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: './public/icon-192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        }
+      ]
     },
 
     injectManifest: {
@@ -30,12 +50,37 @@ export default defineConfig({
     },
 
     devOptions: {
-      enabled: false,
+      enabled: true,
       navigateFallback: 'index.html',
       suppressWarnings: true,
       type: 'module',
     },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      runtimeCaching: [
+        {
+          urlPattern: /\.(?:png|svg|ico|webp)$/,
+          handler: 'CacheFirst',
+        },
+        {
+          urlPattern: /.*\.html.*/,
+          handler: 'CacheFirst',
+        },
+        {
+          urlPattern: /.*\.css.*/,
+          handler: 'CacheFirst',
+        },
+        {
+          urlPattern: /.*\.js.*/,
+          handler: 'CacheFirst',
+        },
+      ],
+    },
   })],
+  server: {
+    host: '0.0.0.0',
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
